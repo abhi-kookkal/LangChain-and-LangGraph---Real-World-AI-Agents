471a927debea7fffa165bf8a6032d643d297f30f
abhijith055 <abhijith034@gmail.com>
2025-10-28 15:50:42 +0530

    used with_structured_output instead of pydantic output parser

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ load_dotenv()
 
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_ollama import ChatOllama
@@ -16,11 +15,11 @@ from schemas import AgentResponse
 tools = [TavilySearch()]
 # llm = ChatOllama(model="gemma3:270m")
 llm = ChatOllama(model="mistral")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+structured_llm = llm.with_structured_output(AgentResponse) 
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"]
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 
 agent = create_react_agent(
@@ -31,13 +30,10 @@ agent = create_react_agent(
 agent_executor = AgentExecutor(
     agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
 )
-extract_otput = RunnableLambda(
+extract_output = RunnableLambda(
     lambda x: x["output"]
 )
-parse_output = RunnableLambda(
-    lambda x: output_parser.parse(x)
-)
-chain = agent_executor | extract_otput | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
