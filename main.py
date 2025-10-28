a1192a5aa439c515f6d1bf732a2b7847211f730d
abhijith055 <abhijith034@gmail.com>
2025-10-28 14:54:37 +0530

    added output parser functionality using pydantic schemas

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from dotenv import load_dotenv
 
-from langchain_classic import hub
+load_dotenv()
+
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
-load_dotenv()
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 # llm = ChatOllama(model="gemma3:270m")
 llm = ChatOllama(model="mistral")
-react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"]
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt,
+    prompt= react_prompt_with_format_instructions,
 )
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+agent_executor = AgentExecutor(
+    agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+)
 chain = agent_executor
+
 
 def main():
     result = chain.invoke(
@@ -27,6 +41,7 @@ def main():
         }
     )
     print(result)
+
 
 if __name__ == "__main__":
     main()
